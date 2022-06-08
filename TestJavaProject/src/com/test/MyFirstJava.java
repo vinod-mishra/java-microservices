cfe665f52eec5fa0cf352c23bc6d3a0f29a87c48
MAC <MAC@localhost>
2022-06-08 17:51:06 +0530

    First Java Project

---
+++ TestJavaProject/src/com/test/MyFirstJava.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class MyFirstJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My First Java Program");
+		
+	}
+
+}
